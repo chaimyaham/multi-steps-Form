a58f86b0c72243b8b152c52fae039c924608f3b2
chaimyaham <chaim.yaham17@gmail.com>
2023-07-17 18:35:40 +0100

    script modification

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const year=document.getElementById('year')
 const monthly=document.getElementById('monthly')
-const next=document.getElementById('next')
-const back=document.getElementById('back')
-const step2=document.querySelector('.step2')
-const step1=document.querySelector('.step1')
+
+
 const num=document.querySelectorAll('.num')
+const step=document.querySelectorAll('.step')
+const nextbtn=document.querySelectorAll('.next')
+const backbtn=document.querySelectorAll('.back')
 
 year.addEventListener('change',()=>{
    
@@ -18,28 +19,41 @@ year.addEventListener('change',()=>{
  
   
 })
-next.addEventListener('click',()=>{
-    step2.style.opacity="1"
-    step2.style.transform="translateX(0%)"
-    step2.style.transition="all .5s ease-in-out"
-    step1.style.opacity="0"
-    step1.style.transform="translateX(-50%)"
-    step1.style.transition="all .5s ease-in-out"
+nextbtn.forEach((elt,index) => {
+    elt.addEventListener("click",()=>{
 
-    num[0].classList.remove("active")
-    num[1].classList.add("active")
+        step[index+1].style.opacity="1"
+           step[index+1].style.transform="translateX(0%)"
+           step[index+1].style.transition="all .5s ease-in-out"
+            step[index].style.opacity="0"
+           step[index].style.transform="translateX(-50%)"
+           step[index].style.transition="all .5s ease-in-out"
+           
+           num[index].classList.remove("active")
+           num[index+1].classList.add("active")
+    })
+    
+});
+
+backbtn.forEach((elt,index) => {
+    elt.addEventListener("click",()=>{
+console.log(index)
+        step[index].style.opacity="1"
+           step[index].style.transform="translateX(0%)"
+           step[index].style.transition="all .5s ease-in-out"
+            step[index+1].style.opacity="0"
+           step[index+1].style.transform="translateX(100%)"
+           step[index+1].style.transition="all .5s ease-in-out"
+           
+           num[index+1].classList.remove("active")
+           num[index].classList.add("active")
+    })
+    
+});
 
 
-})
-back.addEventListener('click',()=>{
-    step1.style.opacity="1"
-    step1.style.transform="translateX(0%)"
-    step1.style.transition="all .5s ease-in-out"
-    step2.style.opacity="0"
-    step2.style.transform="translateX(100%)"
-    step2.style.transition="all .5s ease-in-out"
 
-    num[1].classList.remove("active")
-    num[0].classList.add("active")
+    
 
-})
+
+
