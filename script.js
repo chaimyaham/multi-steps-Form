13a44d4ec50d8a79791a365ae115570d669bc9d3
chaimyaham <chaim.yaham17@gmail.com>
2023-07-18 22:10:38 +0100

    step 3 modidfication

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
 const year=document.getElementById('year')
 const monthly=document.getElementById('monthly')
-
+const hiddenPara=document.querySelectorAll('.hiddenPara')
+const planeName=document.getElementById('plane-name')
+const planAmount=document.getElementById('plan-amount')
+const planType=document.getElementById('planType')
+const verification=document.querySelector('.verification')
+const radioPlan = document.querySelectorAll('input[name="plan"]');
+const add = document.querySelectorAll('input[name="add"]');
 
 const num=document.querySelectorAll('.num')
 const step=document.querySelectorAll('.step')
 const nextbtn=document.querySelectorAll('.next')
 const backbtn=document.querySelectorAll('.back')
+const  yearInfo=document.querySelectorAll('.yearInfo')
+const more=document.querySelectorAll('.more')
+
+
+
+console.log(add)
+
+
 
 year.addEventListener('change',()=>{
    
    if(monthly.style.color===""||monthly.style.color==="rgb(2, 41, 90)"){
+       planType.innerText="(yearly)"
+       console.log(planType.innerText)
         monthly.style.color="#9699ab"
+        yearInfo[0].innerText="$90/yr"
+        yearInfo[1].innerText="$120/yr"
+        yearInfo[2].innerText="$150/yr"
+        more[0].innerText="+$10/yr"
+        more[1].innerText="+$20/yr"
+        more[2].innerText="+$20/yr"
+
+        
+        hiddenPara.forEach(elt => {
+            elt.style.display="block"
+
+            
+        });
+       
+       
      
     }else{
         monthly.style.color="#02295a"
-   
+        yearInfo[0].innerText="$9/mo"
+        yearInfo[1].innerText="$12/mo"
+        yearInfo[2].innerText="$15/mo"
+        more[0].innerText="+$1/mo"
+        more[1].innerText="+$2/mo"
+        more[2].innerText="+$2/mo"
+        planType.innerText="(monthly)"
+        console.log(planType.innerText)
+        hiddenPara.forEach(elt => {
+            elt.style.display="none"
+
+            
+        });
     }
  
   
@@ -24,7 +67,7 @@ nextbtn.forEach((elt,index) => {
         const inputs=[...step[index].querySelectorAll('input')]
         const allValid=inputs.every(input =>input.checkValidity())
         const labels=[...step[index].querySelectorAll('label')]
-        console.log(labels)
+       
         if(allValid){
             step[index+1].style.opacity="1"
             step[index+1].style.transform="translateX(0%)"
@@ -42,6 +85,28 @@ nextbtn.forEach((elt,index) => {
              }
                 elt.style.border="2px solid rgb(2, 41, 90)"
             })
+
+            radioPlan.forEach((element,index)=> {
+    
+                if(element.checked){
+                    planeName.innerText=element.value
+                    planAmount.innerText=yearInfo[index].innerText
+                
+                }
+                
+            });
+            add.forEach((element,index)=> {
+    
+                if(element.checked){
+                    
+                    
+                
+                }else{
+                    
+                }
+                
+            });
+
         }else{
             inputs.forEach((elt,i)=>{
                const valid= elt.checkValidity()
